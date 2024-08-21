faf89951a34d01b992d52f16ee7f1f253f287a10
Muhammad Nur Alfian Syarif <222112218@stis.ac.id>
2024-08-21 14:33:37 +0700

    Feat: layout guest

---
+++ resources/views/page/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.guest')
+
+@section('content')
+   <p class="text-primary">tess</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('page.home');
+});
 
 Route::get('/categories', [CategoryController::class, 'index']);
