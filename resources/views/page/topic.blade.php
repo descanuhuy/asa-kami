f9eeeb075f642071d150f28b07ce9f917989901b
222111943 <222111943.stis.ac.id>
2024-08-22 14:38:13 +0700

    dari rara

--- resources/views/page/topic.blade.php
+++ resources/views/page/topic.blade.php
@@ -1,18 +1,8 @@
 @extends('layouts.guest')
 
 @section('content')
-<header class="py-5 bg-light border-bottom mb-4" style="background-image: url('image/bps_tuban.jpg'); background-size: cover; background-position: center; margin-top: 4.9em;">
-    <div class="container" style="height: 17em; margin-top: 8em;">
-        <div class="text-center my-5">
-            <div style="background-color: rgba(0, 0, 0, 0.5); padding: 1em; border-radius: 10px; display: inline-block;">
-                <h1 class="fw-bolder" style="color: white;">Selamat datang di ASA KITA!</h1>
-                <p class="lead mb-0" style="color: white;">A Bootstrap 5 starter layout for your next blog homepage</p>
-            </div>
-        </div>
-    </div>
-</header>
 
-  <style>
+<style>
     .news-link {
         color: inherit;
         text-decoration: none;
@@ -94,34 +84,5 @@
       </div>
     </div>
   </div>
-
-<!-- Sidebar Column -->
-<div class="col-md-4">
-  <!-- Berita Lainnya Card -->
-  <div class="card mb-4" style="width: 100%; height: auto;">
-    <div class="card-header">
-      <strong>Lainnya!</strong>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item"><a href="#" class="news-link">News Title 1</a></li>
-      <li class="list-group-item"><a href="#" class="news-link">News Title 2</a></li>
-      <li class="list-group-item"><a href="#" class="news-link">News Title 3</a></li>
-      <li class="list-group-item"><a href="#" class="news-link">News Title 4</a></li>
-      <li class="list-group-item"><a href="#" class="news-link">News Title 5</a></li>
-    </ul>
-  </div>
-
-  <!-- Statistik Pengunjung Card -->
-  <div class="card" style="width: 100%; height: auto;">
-    <div class="card-header">
-       <strong>Statistik Pengunjung</strong>
-    </div>
-    <div class="card-body">
-      <p class="card-text">Total Pengunjung: 1200</p>
-      <p class="card-text">Pengunjung Hari Ini: 150</p>
-       <p class="card-text">Pengunjung Bulan Ini: 4500</p>
-    </div>
-</div>
-
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('page.home');
 });
 
-Route::get('/home', function () {
-    return view('page.home');
+Route::get('/topic', function () {
+    return view('page.topic');
+});
+
+Route::get('/archive', function () {
+    return view('page.archive');
+});
+
+Route::get('/about', function () {
+    return view('page.about');
+});
+
+Route::get('/news', function () {
+    return view('page.news');
 });
 
 Route::get('/categories', [CategoryController::class, 'index']);
