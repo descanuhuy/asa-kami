135bfabe78b5e47c9f29b8199e408f857e9a5d5b
arlitacans <212111923@stis.ac.id>
2024-09-03 07:57:17 +0700

    update bisa tuntas

--- resources/views/page/about.blade.php
+++ resources/views/page/about.blade.php
@@ -4,32 +4,33 @@
     <div class="container bg-light rounded-custom p-5" style="margin: 100px 0px;">
         <div class="row">
             <div class="col-md-3">
-                <h2 style="font-size: 28px;" class="mb-3">Tentang ASA KITA</h2>
+                <h2 style="font-size: 28px;" class="mb-3">Tentang BISA TUNTAS</h2>
                 <div class="list-group" id="sidebar-menu">
-                    <a href="#" class="list-group-item list-group-item-action active" data-content="mengapa">Mengapa Harus ASA KITA?</a>
-                    <a href="#" class="list-group-item list-group-item-action" data-content="sasaran">Sasaran dan Tujuan</a>
+                    <a href="#" class="list-group-item list-group-item-action active" data-content="mengapa">Latar Belakang</a>
+                    <a href="#" class="list-group-item list-group-item-action" data-content="sasaran">Tujuan dan Sasaran</a>
                     <a href="#" class="list-group-item list-group-item-action" data-content="strategi">Strategi</a>
                     <a href="#" class="list-group-item list-group-item-action" data-content="hasil">Hasil yang Diharapkan</a>
                 </div>
             </div>
             <div class="col-md-9">
                 <div id="mengapa" class="content-section" style="text-align: justify;">
-                    <h3 style="font-size: 23px;" class="mb-3">Mengapa harus ASA KITA?</h3>
-                    <p><strong>PEMBIN<span style="color:red;font-weight:bold">A</span>AN <span style="color:red;font-weight:bold">S</span>TATISTIK SEKTOR<span style="color:red;font-weight:bold">A</span>L BER<span style="color:red;font-weight:bold">K</span>UAL<span style="color:red;font-weight:bold">I</span>TAS DAN BERKELANJU<span style="color:red;font-weight:bold">TA</span>N </strong></p>
-                    <p>Badan Pusat Statistik (BPS) memiliki peran penting sebagai Pembina Data, namun sayangnya, 
-                        pembinaan statistik sektoral belum dilakukan secara rutin. Selain itu, Organisasi Perangkat Daerah (OPD) yang menyelenggarakan kegiatan statistik masih belum sepenuhnya mengajukan rekomendasi statistik dan menyampaikan metadata ke BPS Kabupaten Tuban. 
-                        Kondisi ini tercermin dalam rendahnya nilai hasil Evaluasi Pengelolaan Statistik Sektoral (EPSS) Pemkab Tuban tahun 2023, yang hanya mencapai nilai IPS sebesar 2,05. Tantangan lain yang dihadapi adalah terbatasnya jumlah statistisi serta adanya mutasi 
-                        penanggung jawab data dan statistik yang cepat di OPD, yang turut mempengaruhi kualitas pengelolaan data statistik.</p>
-                    <p>Untuk itu, Pembinaan statistik sektoral yang berkualitas dan berkelanjutan sangat penting untuk memastikan akurasi dan integritas data.</p>
+                    <h3 style="font-size: 23px;" class="mb-3">Mengapa harus BISA TUNTAS?</h3>
+                    <p><strong>PEM<span style="color:red;font-weight:bold">BI</span>NAAN <span style="color:red;font-weight:bold">S</span>T<span style="color:red;font-weight:bold">A</span>TISTIK SEK<span style="color:red;font-weight:bold">T</span>ORAL YANG BERKELANJ<span style="color:red;font-weight:bold">U</span>TA<span style="color:red;font-weight:bold">N </span>DAN BERKUALI<span style="color:red;font-weight:bold">TAS </strong></p>
+                    <p>Badan Pusat Statistik (BPS) memiliki peran penting sebagai Pembina Data. Namun sayangnya, 
+                        penyelenggaraan pembinaan statistik sektoral di Kabupaten Tuban belum optimal. Hal ini menyebabkan belum semua OPD mendapatkan pembinaan statistik sesuai kebutuhan,
+                        pengajuan rekomendasi statistik dan penyampaian metadata masih rendah,
+                        nilai Indeks Pembangunan Statistik (IPS) juga masih rendah,
+                        belum tersedianya materi pembinaan statistik sektoral yang memuat indikator kematangan EPSS, serta
+                        belum ada pendokumentasian kegiatan pembinaan statistik sektoral yang bisa dimanfaatkan oleh pihak eksternal. </p>
+                        
+                    <p>Untuk itu, implementasi web "BISA TUNTAS" merupakan aksi perubahan untuk mendukung pembinaan statistik sektoral yang berkelanjutan dan berkualitas di Kabupaten Tuban.</p>
                 </div>
                 <div id="sasaran" class="content-section" style="display:none;">
-                    <h3 style="font-size: 23px;" class="mb-3">Sasaran dan Tujuan</h3>
+                    <h3 style="font-size: 23px;" class="mb-3">Tujuan dan Sasaran</h3>
                     <ol>
-                        <li>Terselenggaranya pembinaan statistik sektoral secara rutin 1x/bulan dengan materi yang telah disusun berdasarkan indikator kematangan EPSS</li>
-                        <li>Meningkatnya pemahaman statistik perangkat OPD</li>
-                        <li>Tingkat partisipasi OPD dalam kegiatan pembinaan rutin sebesar 100 persen</li>
-                        <li>OPD yang menyelenggarakan kegiatan statistik telah mengajukan rekomendasi dan metadata statistik sebesar 100 persen pada tahun 2025</li>
-                        <li>Meningkatnya nilai Indeks Pembangunan Statistik Pemkab Tuban pada tahun 2025</li>
+                        <li>Penyelenggaraan pembinaan statistik sektoral tercapai 100 persen</li>
+                        <li>Rekomendasi dan penyampaian metadata Statistik tercapai 100 persen</li>
+                        <li>Nilai Indeks Pembangunan Statistik Pemkab Tuban meningkat pada tahun 2025</li>
                     </ol>
                 </div>
                 <div id="strategi" class="content-section" style="display:none;">
@@ -37,20 +38,20 @@
                     <div>
 
                         <ol>
-                            <li>Menyusun matriks alokasi statistisi sebagai pendamping statistik pada setiap OPD</li>
-                            <li>Menyusun materi pembinaan statistik berdasarkan indikator kematangan EPSS</li>
-                            <li>Kolaborasi dengan Dinas Kominfo SP Kab Tuban dan Bappeda dalam pelaksanaan ASA KITA</li>
-                            <li>Mendokumentasikan seluruh kegiatan dan materi pembinaan statistik sektoral untuk manfaat yang lebih luas</li>
+                            <li>Mengubah perilaku dari minat yang rendah menuju minat yang tinggi untuk meningkatkan pembinaan statistik sektoral yang berkelanjutan dan berkualitas</li>
+                            <li>Memperoleh dukungan, baik dari pimpinan, tim kerja, maupun pihak eksternal</li>
+                            <li>Menggunakan sumber daya manusia yang berkualitas, peralatan yang mumpuni, dan dukungan finansial yang baik</li>
                             <ol>
                     </div>
                     
                     <div>
                     
-                    <p>Selanjutnya, untuk terus mengembangkan program ASA KITA, dilakukan evaluasi berdasarkan feedback yang diterima. Cara kami mendapatkan feedback adalah sebagai berikut:</p>
+                    <p>Selanjutnya, untuk terus mengembangkan program BISA TUNTAS, dilakukan evaluasi berdasarkan feedback yang diterima. Cara kami mendapatkan feedback adalah sebagai berikut:</p>
                     <ol>
+                        <li>Menyebarkan angket evaluasi web BISA TUNTAS</li>
                         <li>Memberikan pretest dan postest pada saat melakukan pembinaan statistik sektoral</li>
-                        <li>Memberikan angket evaluasi di setiap akhir pelaksanaan pembinaan statistik sektoral</li>
-                        <li>Berkoordinasi dengan Dinas Kominfo SP dan Bappeda Litbang terkait kebutuhan pembinaan statistik yang dibutuhkan oleh OPD</li>
+                        <li>Evaluasi kebutuhan pembinaan statistik</li>
+                        <li>Monitoring berkala</li>
                     <ol>
                 </div>
                 <div>            
@@ -59,8 +60,8 @@
                 <div id="hasil" class="content-section" style="display:none;">
                     <h3 style="font-size: 23px;" class="mb-3">Hasil yang Diharapkan</h3>
                     <ol>
-                        <li>Meningkatkan pengetahuan dan pemahaman statistik perangkat OPD</li>
                         <li>Terselenggaranya kegiatan statistik sektoral sesuai dengan prinsip SDI</li>
+                        <li>Dampak perubahan dirasakan oleh semua pihak</li>
                         <li>Meningkatnya nilai Indeks Pembangunan Statistik (IPS) Pemkab Tuban</li>
                     </ol>
                 </div>
--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -7,14 +7,13 @@
     <div class="container" style="height: 17em; margin-top: 8em;">
         <div class="text-center my-5">
             <div style="background-color: rgba(0, 0, 0, 0.5); padding: 1em; border-radius: 10px; display: inline-block;">
-                <h1 class="fw-bolder" style="color: white;">Selamat datang di ASA KITA!</h1>
-                <p class="lead mb-0" style="color: white;">A Bootstrap 5 starter layout for your next blog homepage</p>
+                <h1 class="fw-bolder" style="color: white;">Selamat datang di BISA TUNTAS!</h1>
+                <p class="lead mb-0" style="color: white;">Upaya Peningkatan Pembinaan Statistik Sektoral yang Berkelanjutan dan Berkualitas</p>
             </div>
         </div>
     </div>
 </header> 
 
-  
 <div class="container my-5">
 
     <div class="row gx-5">
@@ -28,58 +27,50 @@
           <ul class="list-group list-group-flush">
             @if (count($otherPosts) === 0)
               <p class="text-center text-danger fs-5">Tidak ada berita lainnya</p>
+                    @else
+                        @foreach ($otherPosts as $item)
+                            <li class="list-group-item"><a href="{{ route('news', ['id' => $item['id']]) }}" class="news-link">{{ $item['title'] }}</a></li>
+                        @endforeach
+                    @endif
+                </ul>
+            </div>
 
-            @else
-              @foreach ($otherPosts as $item)
-                <li class="list-group-item"><a href="{{ route('news', ['id' => $item['id']]) }}" class="news-link">{{ $item['title'] }}</a></li>
-              @endforeach
-            @endif
-          
-          </ul>
-        </div>
-
-        <!-- Statistik Pengunjung Card -->
-        <div class="card" style="width: 100%; height: auto;">
-          <div class="card-header" style="background-color: #55679C; color: white">
-             <strong>Statistik Pengunjung</strong>
-          </div>
-          <div class="card-body">
-            <p class="card-text">Total Pengunjung: 1200</p>
-            <p class="card-text">Pengunjung Hari Ini: 150</p>
-             <p class="card-text">Pengunjung Bulan Ini: 4500</p>
-          </div>
-        </div>
-
-      </div>
-
-      <div class="col-8">
-        <div class="row row-cols-1 row-cols-md-2 bg-light px-4 py-5 rounded-custom">
-          
-            @foreach ($mainPosts as $post)
-              <div class="col">
-                <div class="card mb-4">
-                    <img src="{{ $post['thumbnail'] }}" alt="Thumbnail" style="height: 180px; margin-right: 10px;" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $post['title'] }}</h5>
-                        <p class="card-text">{!! Str::limit($post['content'], 15) !!}</p>
-                        <a href="{{ route('news', ['id' => $post['id']]) }}" class="d-flex card-text news-link selengkapnya">Selengkapnya...</a>
-                    </div>
+            <!-- Statistik Pengunjung Card -->
+            <div class="card" style="width: 100%; height: auto;">
+                <div class="card-header" style="background-color: #55679C; color: white">
+                    <strong>Statistik Pengunjung</strong>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Total Pengunjung: 1200</p>
+                    <p class="card-text">Pengunjung Hari Ini: 150</p>
+                    <p class="card-text">Pengunjung Bulan Ini: 4500</p>
                 </div>
             </div>
-            @endforeach
-
-
-            @if (!$mainPosts)
-                <div class="alert alert-danger" role="alert">
-                    Belum ada berita
-                </div>
-            @endif
-
         </div>
-      </div>
 
+        <div class="col-8">
+            <div class="row row-cols-1 row-cols-md-2 bg-light px-4 py-5 rounded-custom">
+                @foreach ($mainPosts as $post)
+                    <div class="col">
+                        <div class="card mb-4" style="height: 100%; width: 100%;">
+                            <img src="{{ $post['thumbnail'] }}" alt="Thumbnail" style="height: 180px; width: 100%; object-fit: cover;" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $post['title'] }}</h5>
+                                <p class="card-text">{!! Str::limit($post['content'], 15) !!}</p>
+                                <a href="{{ route('news', ['id' => $post['id']]) }}" class="d-flex card-text news-link selengkapnya">Selengkapnya...</a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+
+                @if (!$mainPosts)
+                    <div class="alert alert-danger" role="alert">
+                        Belum ada berita
+                    </div>
+                @endif
+            </div>
+        </div>
     </div>
-        
 </div>
 
 @endsection
